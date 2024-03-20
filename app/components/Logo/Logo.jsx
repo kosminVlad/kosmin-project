f01a48cf9d81b6a5ee7d0dcc0924b31c60013a72
kosmin <ksagwey.vlad@mail.ru>
2024-03-20 17:59:48 +0700

    123

--- app/components/Logo/Logo.jsx
+++ app/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import Styles from "./Logo.module.css"
 export const Logo = () => {
   return (
     <div className={Styles["logo"]}>
-    <h1>KOSMIN</h1>
+    <h1 className={Styles["logo-h1"]}>KOSMIN</h1>
  </div>
   )
 }
--- app/components/Menu/Menu.jsx
+++ app/components/Menu/Menu.jsx
@@ -7,8 +7,8 @@ import { Logo } from '../Logo/Logo'
 export const Menu = () => {
   return (
     <menu className={Styles['menu']}>
+              <Logo/>
       <div className={Styles["menu-content"]}>
-        <Logo/>
         <div className={Styles["list"]}>
           <ul className={Styles["menu__list"]}>
             <li className={Styles["menu__list-li"]}> <Link href="/" className={Styles["menu__list-url"]}> PROJECTS </Link> </li>
@@ -17,16 +17,16 @@ export const Menu = () => {
             <li className={Styles["menu__list-li"]} ><Link href="/" className={Styles["menu__list-url"]}> CLIPS </Link> </li>
           </ul>
           <ul className={Styles["menu__urls"]}>
-            <a href="https://vk.com/kosminvlad">
-              <li className={Styles["menu__urls-url"]}></li>
-            </a>
-            <a href="https://t.me/kosmin0">
-              <li className={Styles["menu__urls-url"]} id="telegram"></li>
-            </a>
-            <li className={Styles["menu__urls-url"]} id="discord"></li>
-            <a href="https://github.com/kosminVlad">
-              <li className={Styles["menu__urls-url"]}></li>
-            </a>
+            <Link href="https://vk.com/kosminvlad">
+              <li className={Styles["menu__urls-url"]}> <img src="./images/VK Logo White.png" alt="dsadsa" className={Styles["menu__urls-img"]}/> </li>
+            </Link>
+            <Link href="https://t.me/kosmin0">
+              <li className={Styles["menu__urls-url"]} id="telegram"> <img src="./images/telegram-xxl.png" alt="dsadsa" className={Styles["menu__urls-img"]}/> </li>
+            </Link>
+            <li className={Styles["menu__urls-url"]} id="discord"> <img src="./images/discord-mark-white.png" alt="dsadsa" className={Styles["menu__urls-img"]}/> </li>
+            <Link href="https://github.com/kosminVlad">
+              <li className={Styles["menu__urls-url"]}> <img src="./images/github-mark-white.png " alt="dsadsa" className={Styles["menu__urls-img"]}/> </li>
+            </Link>
           </ul>
         </div>
       </div>
